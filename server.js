ee46b6b70d575e1d0fdefce11fcc8868d7157393
JoseCabreraJC <josem.cabrera259@gmail.com>
2021-06-30 00:16:39 -0300

    pushed initial state

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Estamos deployados! Em vivo");
+  res.end("Estamos deployados!");
 });
 
 server.listen(port, () => {
